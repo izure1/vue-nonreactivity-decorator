934f3c6a6923bca177f07a1399a44f2d363b5748
izure <=>
2021-01-14 15:12:20 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Component } from 'vue'
+
+const NON_REACTIVITY: WeakMap<Vue, Map<string, any>> = new WeakMap
+
+function ensureMap(component: Vue): Map<string, any> {
+    if (!NON_REACTIVITY.has(component)) {
+        NON_REACTIVITY.set(component, new Map)
+    }
+    return NON_REACTIVITY.get(component)!
+}
+
+function setNonReactivity(component: Vue, key: string, value: any): void {
+    ensureMap(component).set(key, value)
+}
+
+function getNonReactivity(component: Vue, key: string): any {
+    return ensureMap(component).get(key)
+}
+
+export default function NonReactivity(target: Vue, property: string): void {
+    Object.defineProperty(target, property, {
+        get() {
+            return getNonReactivity(target, property)
+        },
+        set(v: any) {
+            setNonReactivity(target, property, v)
+        }
+    })
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'index': [
+            '@babel/polyfill',
+            path.resolve(__dirname, 'src', 'index.ts')
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    externals: [
+        'vue'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-typescript'
+                            ],
+                            plugins: [
+                                '@babel/plugin-proposal-class-properties'
+                            ]
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'node_modules')
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    }
+}
